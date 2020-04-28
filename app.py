63c85f6901c5e70eaf1e30ee88a5ba48becc7f47
sampada-sakpal <41463494+sampada-sakpal@users.noreply.github.com>
2020-04-28 15:46:06 +1000

    Update app.py
    read file contents

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/read_file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return contents
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
